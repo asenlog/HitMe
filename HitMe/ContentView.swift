1c14705f4e4ba63c174fb914a48972a4d3ce7ecf
Angelos Roussakis <aggelos.roussakis@gmail.com>
2020-01-02 22:16:08 +0200

    Initial Commit

---
+++ HitMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HitMe
+//
+//  Created by Angelos Roussakis on 2/1/20.
+//  Copyright © 2020 Angelos Roussakis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
